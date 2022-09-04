97f0390e404388cae3509572f2b8b5c9e777c5d4
chopeen <chopeen@gmail.com>
2022-09-04 21:57:17 +0200

    Output debugging information from healthcheck

--- docker-healthcheck.sh
+++ docker-healthcheck.sh
@@ -3,7 +3,9 @@
 SYSPROPS_FILE=${DATADIR}/system.properties
 if [ -f "${SYSPROPS_FILE}" ]; then
     SYSPROPS_PORT=`grep "^unifi.https.port=" ${SYSPROPS_FILE} | cut -d'=' -f2`
+    echo "SYSPROPS_PORT: ${SYSPROPS_PORT}"
 fi
 PORT=${SYSPROPS_PORT:-8443}
+echo "PORT: ${PORT}"
 
-curl --max-time 5 -kILs --fail https://localhost:${PORT}
+curl --max-time 5 -kIL --fail https://localhost:${PORT}
