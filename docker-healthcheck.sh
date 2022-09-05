d44faea1c2e27a1fa167eadfec13619e3375d86b
chopeen <chopeen@gmail.com>
2022-09-05 22:05:39 +0200

    Continue checking the logs

--- docker-healthcheck.sh
+++ docker-healthcheck.sh
@@ -10,8 +10,13 @@ echo "PORT: ${PORT}"
 
 curl --max-time 5 -kIL --fail https://localhost:${PORT}
 
-ls /var/
-ls /var/log/
-ls /usr/lib/unifi/logs/
+echo "*** 1 ***"
+ls /var/log/mongodb/
+echo "*** 2 ***"
+ls /var/log/unifi/
+echo "*** 3 ***"
+cat /usr/lib/unifi/logs/migration.log
+echo "*** 4 ***"
+cat /usr/lib/unifi/logs/server.log
 
 exit 1
